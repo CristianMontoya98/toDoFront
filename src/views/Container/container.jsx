7db21b0b755bbf067505cb19c35eaa1e415ca544
andreavdiaz <andreavdiaz1994@gmail.com>
2022-03-03 15:00:46 -0500

    Footercomplete

--- src/views/Container/container.jsx
+++ src/views/Container/container.jsx
@@ -1,11 +1,14 @@
+import Footer from "./components/Footer/Footer";
 import { NavBar } from "./components/NavBar/navBar";
 import { Tasks } from "./components/Tasks/Tasks";
+
 
 function Container() {
     return (
         <div>
             <NavBar />
             <Tasks/>
+            <Footer/>
         </div>
 
     );
