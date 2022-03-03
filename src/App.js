3412eaedd3c7f06a3186f385f524697b155296b0
Christian Montoya <chrimonlo@gmail.com>
2022-03-02 23:27:11 -0500

    Restructuring complete using atomic design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React, { useState } from 'react';
-import Form from "./components/FORM/Form";
+/* import React, { useState } from 'react'; */
+import { Container } from './views/Container/container';
 
-
-import Tasks from './components/TASKS/Tasks';
-
-
-
-
-const appTitle = "Gestor de Tareas"
-
-const list = [
+/* const list = [
   {id: 1,title: "tarea1 ", description:"lorem ipsum",date:"2022-10-23",state:"incompleted",deleted:false},
   {id: 2,title:"tarea 2",description:"lorem ipsum",date:"2022-10-23",state:"incompleted",deleted:false},
   {id:3, title:"tarea 2",description:"lorem ipsum",date:"2022-10-23",state:"incompleted",deleted:false},
 
-]
+] */
 
 const  App=()=>{
 
- const [todoList, setTodoList] = useState(list)
+ /* const [todoList, setTodoList] = useState(list)
 
  const addTodo = item =>{
    setTodoList((oldList)=>[...oldList,item])
@@ -29,12 +21,12 @@ const  App=()=>{
     const changesTodos = todoList.splice (todo => todo.id !== id);
 
     setTodoList(changesTodos);
- };
+ }; */
 
   return (
     <div className="App">
      
-          <h1>{appTitle}</h1>
+        {/*   <h1>{appTitle}</h1>
     
 
       
@@ -42,7 +34,8 @@ const  App=()=>{
      
 
      
-        <Tasks removeTodoListProp={removeTodo} list={todoList} />
+        <Tasks removeTodoListProp={removeTodo} list={todoList} /> */}
+      <Container/>
       
 
     </div>
---
+++ src/views/Container/components/Tasks/components/AddBox/AddBox.jsx
@@ -0,0 +1,12 @@
+function AddBox() {
+  return (
+    <div>
+      <form action="">
+        <input type="text" />
+        <input type="text" />
+        <button type="submit">Agregar</button>
+      </form>
+    </div>
+  );
+}
+export { AddBox };
