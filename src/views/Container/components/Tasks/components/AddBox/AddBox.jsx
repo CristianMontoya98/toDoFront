84f06f458ea4cb0d32a0cdb7392aedca9e949ad6
Sebastian Murcia <smurcia162@gmail.com>
2022-03-03 15:01:15 -0500

    function Navbar

--- src/views/Container/components/Tasks/components/AddBox/AddBox.jsx
+++ src/views/Container/components/Tasks/components/AddBox/AddBox.jsx
@@ -1,10 +1,35 @@
+import React, { useState } from 'react'
+import styles from "./styles.module.css"
 function AddBox() {
+
+  const [addTodoTitle, setaddTodoTitle] = useState("");
+  const [addTodoDescription, setaddTodoDescription] = useState("");
+  const handletitle =({target:{value}}) => setaddTodoTitle(value);
+  const handleDescription = ({target:{value}}) => setaddTodoDescription (value)
+
+  console.log(addTodoTitle);
+  console.log(addTodoDescription);
+
   return (
-    <div>
-      <form action="">
-        <input type="text" />
-        <input type="text" />
-        <button type="submit">Agregar</button>
+    <div className={styles.container}>
+      <form action="" className={styles.cont__Info}>
+        <input 
+          type="text"
+          placeholder="Titulo"
+          onChange={handletitle}
+          value={addTodoTitle}
+          className ={styles.input__title}
+         />   
+        <textarea
+          
+          placeholder="Descripcion"
+          value={addTodoDescription}
+          onChange={handleDescription}
+          className ={styles.input__description}
+          
+          />
+        
+        <button type="submit" className={styles.buttonAdd}>Añadir  &#10010;</button>
       </form>
     </div>
   );
