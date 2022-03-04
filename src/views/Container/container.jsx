d18a65de588a3a8cdbcfa1a28de4edc75ed3e7ee
JairoSolarteRodriguez <pvprandom578@gmail.com>
2022-03-03 19:54:11 -0500

    fixed rote Task

--- src/views/Container/container.jsx
+++ src/views/Container/container.jsx
@@ -1,6 +1,6 @@
 import Footer from "./components/Footer/Footer";
 import { NavBar } from "./components/NavBar/navBar";
-import { Tasks } from "./components/Tasks/tasks";
+import { Tasks } from "./components/Tasks/Tasks";
 
 
 function Container() {
